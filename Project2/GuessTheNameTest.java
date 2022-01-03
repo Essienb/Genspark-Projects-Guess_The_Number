a5eae663eb62a075a277c968194476ad71d6d829
Essien Bassey <basseyalison@gmail.com>
2022-01-02 20:55:13 -0600

    Create the Junit5 test class

---
+++ Project2/GuessTheNameTest.java
@@ -0,0 +1,22 @@
+package Project2;
+
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DisplayName("Guess_The_Name-TestCase")
+class GuessTheNameTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
